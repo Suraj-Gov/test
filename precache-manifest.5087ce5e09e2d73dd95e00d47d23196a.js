4d47bad9f171bba1e23d431de25def444e611e3c
Suraj Govind <surajgovind332000@gmail.com>
2020-04-27 17:32:46 +0530

    Updates

---
+++ precache-manifest.5087ce5e09e2d73dd95e00d47d23196a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff15d7a0e7a28287c78c57242d9ed37d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "fdbba02009db136a6cab",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2582694f388c5d50a3ae",
+    "url": "/test/static/js/2.8d0b9438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.8d0b9438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdbba02009db136a6cab",
+    "url": "/test/static/js/main.cb415fc2.chunk.js"
+  },
+  {
+    "revision": "6382f056984a28884ccb",
+    "url": "/test/static/js/runtime-main.a43a3581.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
